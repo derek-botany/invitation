2e4a393f65fefa989d6512e1bb2dc2dc757145b2
derek-botany <5mkjnj156@relay.firefox.com>
2022-12-24 04:33:45 +0000

    can issue close

--- index.js
+++ index.js
@@ -17,9 +17,35 @@ async function checkCollaborators(octokit, thisOwner, thisRepo, thisUsername) {
 async function addCollaborator(octokit, thisOwner, thisRepo, thisUsername) {
     try {
         await octokit.rest.repos.addCollaborator({
-            owner: 'derek-botany',
-            repo: 'invitation',
-            username: 'blackgirlbytes',
+            owner: thisOwner,
+            repo: thisRepo,
+            username: thisUsername,
+          });
+        } catch(error) {
+            console.log(error) 
+         }
+}
+
+async function commentOnIssue(octokit, thisOwner, thisRepo, thisUsername, issueNumber) {
+    try {
+        await octokit.rest.issues.createComment({
+            owner: thisOwner,
+            repo: thisRepo,
+            issue_number: issueNumber,
+            body: `@${thisUsername}, you've been invited to collaborate on this repository. Please accept the invitation in your email or GitHub notifications.`,
+          });
+        } catch(error) {
+            console.log(error) 
+         }
+}
+
+async function closeIssue(octokit, thisOwner, thisRepo, issueNumber) {
+    try {
+        await octokit.rest.issues.update({
+            owner: thisOwner,
+            repo: thisRepo,
+            issue_number: issueNumber,
+            state: 'closed',
           });
         } catch(error) {
             console.log(error) 
@@ -44,23 +70,25 @@ async function run() {
         const thisUsername = issueTitle.match(regex);
         const thisRepo = github.context.payload.issue.html_url
         const thisOwner = github.context.payload.repository.owner.login
+        const issueNumber = github.context.payload.issue.number
      
    
         console.log('Parsed event values:\n\tRepo: ' + thisRepo + '\n\tUsername of commenter: ' +
-            thisUsername + '\n\tRepo Owner: ' + thisOwner + '\n\tIssue number: ');
+            thisUsername + '\n\tRepo Owner: ' + thisOwner);
 
         // check to make sure commenter is not owner (gives big error energy)
         if (thisUsername == thisOwner) {
             console.log('Commenter is the owner of this repository; exiting.');
             process.exit(0);
         } else {
-            console.log('not an owner')
         }
         const isUserCollaborator = await checkCollaborators(octokit, thisOwner, thisRepo, thisUsername)
         console.log('this is user collaborator', isUserCollaborator)
         if (isUserCollaborator == undefined) {
             console.log('we need to add collaborator')
             await addCollaborator(octokit, thisOwner, thisRepo, thisUsername)
+            await commentOnIssue(octokit, thisOwner, thisRepo, thisUsername, issueNumber)
+            await closeIssue(octokit, thisOwner, thisRepo, issueNumber)
         } else {
             console.log('User is already a collaborator; exiting.');
             process.exit(0);
