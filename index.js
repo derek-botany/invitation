92258f15cef5c8d5b4abceadbf25c650062ea793
derek-botany <5mkjnj156@relay.firefox.com>
2022-12-24 17:26:03 +0000

    this should be working

--- index.js
+++ index.js
@@ -2,40 +2,37 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function checkCollaborators(octokit, thisOwner, thisRepo, thisUsername) {
-     
-      const response =  await octokit.rest.repos.checkCollaborator({
-            owner: thisOwner,
-            repo: thisRepo,
-            username: thisUsername,
-          });
-    return response
-   
+    return await octokit.rest.repos.checkCollaborator({
+        owner: thisOwner,
+        repo: thisRepo,
+        username: thisUsername,
+    })
 }
 
-async function addCollaborator(octokit, thisOwner, thisRepo, thisUsername) {
+async function addCollaborator(octokit, owner, repo, username) {
     try {
         await octokit.rest.repos.addCollaborator({
-            owner: thisOwner,
-            repo: thisRepo,
-            username: thisUsername,
-          });
-        } catch(error) {
-            console.log('ERROR: ' + error.message + ' occurred at ' + error.fileName + ':' + error.lineNumber);
-         }
+            owner,
+            repo,
+            username,
+        });
+    } catch (error) {
+        console.log('ERROR: ' + error.message + ' occurred at ' + error.fileName + ':' + error.lineNumber);
+    }
 }
 
-    async function addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment) {
-        try {
-            await octokit.rest.issues.createComment({
-                owner: thisOwner,
-                repo: thisRepo,
-                issue_number: thisIssueNumber,
-                body: comment
-            });
-        } catch (error) {
-            console.log('ERROR: ' + error.message + ' occurred at ' + error.fileName + ':' + error.lineNumber);
-        }
+async function addComment(octokit, owner, repo, issueNumber, comment) {
+    try {
+        await octokit.rest.issues.createComment({
+            owner: owner,
+            repo: repo,
+            issue_number: issueNumber,
+            body: comment
+        });
+    } catch (error) {
+        console.log('ERROR: ' + error.message + ' occurred at ' + error.fileName + ':' + error.lineNumber);
     }
+}
 
 async function closeIssue(octokit, thisOwner, thisRepo, thisIssueNumber) {
     try {
@@ -70,10 +67,9 @@ async function run() {
         if (!thisToken) {
             console.log('ERROR: Token was not retrieved correctly and is falsy.');
             core.setFailed('Error: token was not correctly interpreted');
-            console.log('was it received')
         }
         const octokit = new github.getOctokit(thisToken);
-  
+
         // get comment
         const issueTitle = github.context.payload.issue.title;
         const regex = /(?<=@)\w+/g;
@@ -81,20 +77,19 @@ async function run() {
         const thisRepo = github.context.payload.repository.name
         const thisOwner = github.context.payload.repository.owner.login
         const thisIssueNumber = github.context.payload.issue.number
-     
-   
+
+
         console.log('Parsed event values:\n\tRepo: ' + thisRepo + '\n\tUsername of commenter: ' +
-            thisUsername + '\n\tRepo Owner: ' + thisOwner);
+            thisUsername + '\n\tRepo Owner: ' + thisOwner + '\n\tIssue Number: ' + thisIssueNumber);
 
         // check to make sure commenter is not owner (gives big error energy)
         if (thisUsername == thisOwner) {
             console.log('Commenter is the owner of this repository; exiting.');
             process.exit(0);
-        } 
-        
+        }
+
         const isUserCollaborator = await checkCollaborators(octokit, thisOwner, thisRepo, thisUsername)
-        console.log('what is the value',isUserCollaborator, 'what is the value')
-        if(isUserCollaborator.status == 204){
+        if (isUserCollaborator.status == 204) {
             const comment = `@${thisUsername} is already a member of this repository.`
             const label = `duplicate request`
             await addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment);
@@ -102,15 +97,15 @@ async function run() {
             // close issue
             await closeIssue(octokit, thisOwner, thisRepo, thisIssueNumber);
         } else {
-                await addCollaborator(octokit, thisOwner, thisRepo, thisUsername)
-                // add comment to issue
-                const comment = `@${thisUsername} has been added as a member of this repository. Please check your email or notifications for an invitation.`
-                const label = 'collaborator added'
-                await addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment);
-                // add label to issue
-                await addLabel(octokit, thisOwner, thisRepo, thisIssueNumber, label);
-                // close issue
-                await closeIssue(octokit, thisOwner, thisRepo, thisIssueNumber);
+            await addCollaborator(octokit, thisOwner, thisRepo, thisUsername)
+            // add comment to issue
+            const comment = `@${thisUsername} has been added as a member of this repository. Please check your email or notifications for an invitation.`
+            const label = 'collaborator added'
+            await addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment);
+            // add label to issue
+            await addLabel(octokit, thisOwner, thisRepo, thisIssueNumber, label);
+            // close issue
+            await closeIssue(octokit, thisOwner, thisRepo, thisIssueNumber);
         }
     } catch (error) {
         console.log('ERROR: ' + error.message + ' occurred at ' + error.fileName + ':' + error.lineNumber);
@@ -119,4 +114,4 @@ async function run() {
     }
 }
 
- run()
+run()
