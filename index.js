b3126239ce81c86e1338060b305175bc16234e0e
derek-botany <5mkjnj156@relay.firefox.com>
2022-12-24 05:16:10 +0000

    reverting

--- index.js
+++ index.js
@@ -26,40 +26,6 @@ async function addCollaborator(octokit, thisOwner, thisRepo, thisUsername) {
          }
 }
 
-async function commentOnIssue(octokit, thisOwner, thisRepo, thisUsername, issueNumber) {
-    console.log('here')
-    console.log(thisOwner)
-    console.log(thisRepo)
-    console.log(thisUsername)
-    console.log(issueNumber)
-    // try {
-    //  const response =   await octokit.rest.issues.createComment({
-    //     octokit.rest.issues.createComment({
-    //         owner:thisOwn,
-    //         repo: thisRepo,
-    //         issue_number: issueNumber,
-    //         body: 'hey',
-    //       });
-    //       });
-    //       console.log('comment on issue', response)
-    //     } catch(error) {
-    //         console.log(error) 
-    //      }
-}
-
-async function closeIssue(octokit, thisOwner, thisRepo, issueNumber) {
-    try {
-        await octokit.rest.issues.update({
-            owner: thisOwner,
-            repo: thisRepo,
-            issue_number: issueNumber,
-            state: 'closed',
-          });
-        } catch(error) {
-            console.log(error) 
-         }
-}
-
 async function run() {
     try {
         // create Octokit client
@@ -78,8 +44,7 @@ async function run() {
         const thisUsername = issueTitle.match(regex);
         const thisRepo = github.context.payload.issue.html_url
         const thisOwner = github.context.payload.repository.owner.login
-        const issueNumber = github.context.payload.issue.number
-        console.log(issueNumber)
+     
    
         console.log('Parsed event values:\n\tRepo: ' + thisRepo + '\n\tUsername of commenter: ' +
             thisUsername + '\n\tRepo Owner: ' + thisOwner);
@@ -94,13 +59,7 @@ async function run() {
         console.log('this is user collaborator', isUserCollaborator)
         if (isUserCollaborator == undefined) {
             console.log('we need to add collaborator')
-            const collaboratorAdded = await addCollaborator(octokit, thisOwner, thisRepo, thisUsername)
-            if(collaboratorAdded.status == 204) {
-                console.log('TRY THIS WE ARE HERE!!!')
-                // await commentOnIssue(octokit, thisOwner, thisRepo, thisUsername, issueNumber)
-                // await closeIssue(octokit, thisOwner, thisRepo, issueNumber)
-            }
-          
+            await addCollaborator(octokit, thisOwner, thisRepo, thisUsername)
         } else {
             console.log('User is already a collaborator; exiting.');
             process.exit(0);
