7d99a120fbee081181b3c86271639cee4876c440
derek-botany <5mkjnj156@relay.firefox.com>
2022-12-24 16:53:50 +0000

    test

--- index.js
+++ index.js
@@ -97,15 +97,15 @@ async function run() {
         if(isUserCollaborator.status == 204){
             const comment = `@${thisUsername} is already a member of this repository.`
             const label = `duplicate request`
-            await addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment);
-            await addLabel(octokit, thisOwner, thisRepo, thisIssueNumber, label);
-            // close issue
-            await closeIssue(octokit, thisOwner, thisRepo, thisIssueNumber);
+            // await addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment);
+            // await addLabel(octokit, thisOwner, thisRepo, thisIssueNumber, label);
+            // // close issue
+            // await closeIssue(octokit, thisOwner, thisRepo, thisIssueNumber);
         } else {
                 await addCollaborator(octokit, thisOwner, thisRepo, thisUsername)
                 // add comment to issue
-                const comment = `@${thisUsername} has been added as a member of this repository. Please check your email or notifications for an invitation.`
-                const label = 'collaborator added'
+                // const comment = `@${thisUsername} has been added as a member of this repository. Please check your email or notifications for an invitation.`
+                // const label = 'collaborator added'
                 // await addComment(octokit, thisOwner, thisRepo, thisIssueNumber, comment);
                 // // add label to issue
                 // await addLabel(octokit, thisOwner, thisRepo, thisIssueNumber, label);
